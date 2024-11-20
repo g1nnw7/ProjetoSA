b68a21591f4d22cb4c474a0eb2770b56579c8ca9
dourado840 <fdouradodeczka@gmail.com>
2024-11-19 21:06:54 -0300

    Atualização script 2

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function calcularIMC(){
     altura = document.getElementById("imcaltura").value;
      imc = peso / (altura * altura)
      imc = imc.toFixed(2);
-   document.getElementById("resultado").innerHTML = `O seu IMC é: ${imc}`;
+    document.getElementById("resultado").innerHTML = `O seu IMC é: ${imc}`;
     document.getElementById("imcpeso").value = null;
     document.getElementById("imcaltura").value = null;
+    localStorage.setItem("imcResultado", JSON.stringify(imc));
     event.preventDefault();
    if(imc <= 18.5){
     resultado2 = "Abaixo do Normal"
@@ -27,17 +28,4 @@ function calcularIMC(){
     
 }
 
-function gerarDieta(){
-  let IMC = document.getElementById('imc')
-  if(IMC < 18.5){
-    resultado3 = "Necessário Ganho de peso saudável	- Dieta hipercalórica (acima das necessidades diárias). Focar em alimentos ricos em nutrientes e calorias densas (ex: abacate, castanhas, óleos saudáveis). Incluir proteínas para ganho muscular (ex: carnes magras, ovos, leguminosas). Fracionar as refeições em 5-6 vezes ao dia."
-  } else if(IMC >= 18.5 && IMC <= 24.9){
-    resultado3 = "Dieta equilibrada com a quantidade adequada de calorias, proteínas, carboidratos e gorduras saudáveis. Consumir alimentos ricos em fibras (ex: vegetais, frutas, grãos integrais). 	- Manter uma ingestão de proteínas moderada para manutenção de massa muscular. Manter o consumo de gorduras saudáveis (ex: azeite de oliva, abacate, peixes ricos em ômega-3)."
-  } else if(IMC >= 25 && IMC <= 29.9){
-    resultado2 = "Dieta hipocalórica, com déficit calórico controlado. Focar em alimentos de baixo índice glicêmico (ex: vegetais, legumes, proteínas magras). Reduzir carboidratos refinados (ex: pães brancos, massas, doces). Aumentar a ingestão de fibras e proteínas para maior saciedade (ex: frango, peixe, tofu, lentilhas). Controlar o consumo de gorduras saturadas e aumentar gorduras insaturadas (ex: abacate, azeite de oliva, nozes)."
-  } else if(IMC >= 30 && IMC <= 34.9){
-    resultado3 = "Dieta rigorosamente hipocalórica com acompanhamento profissional.  Aumento do consumo de proteínas para preservar a massa muscular durante o emagrecimento (ex: frango, ovos, peixe). Evitar alimentos altamente processados e ricos em açúcares e gorduras trans. Refeições pequenas e frequentes, controlando porções."
-  }
-     document.getElementById("resultado2").innerHTML = `De acordo com seu IMC: ${ IMC } (${ resultado3 })`;
-  }
 
---
+++ script2.js
@@ -0,0 +1,14 @@
+function gerarDieta(){
+    let IMC = JSON.parse(localStorage.getItem("imcResultado"))
+    if(IMC < 18.5){
+      resultado3 = "Necessário Ganho de peso saudável	- Dieta hipercalórica (acima das necessidades diárias). Focar em alimentos ricos em nutrientes e calorias densas (ex: abacate, castanhas, óleos saudáveis). Incluir proteínas para ganho muscular (ex: carnes magras, ovos, leguminosas). Fracionar as refeições em 5-6 vezes ao dia."
+    } else if(IMC >= 18.5 && IMC <= 24.9){
+      resultado3 = "Dieta equilibrada com a quantidade adequada de calorias, proteínas, carboidratos e gorduras saudáveis. Consumir alimentos ricos em fibras (ex: vegetais, frutas, grãos integrais). 	- Manter uma ingestão de proteínas moderada para manutenção de massa muscular. Manter o consumo de gorduras saudáveis (ex: azeite de oliva, abacate, peixes ricos em ômega-3)."
+    } else if(IMC >= 25 && IMC <= 29.9){
+      resultado2 = "Dieta hipocalórica, com déficit calórico controlado. Focar em alimentos de baixo índice glicêmico (ex: vegetais, legumes, proteínas magras). Reduzir carboidratos refinados (ex: pães brancos, massas, doces). Aumentar a ingestão de fibras e proteínas para maior saciedade (ex: frango, peixe, tofu, lentilhas). Controlar o consumo de gorduras saturadas e aumentar gorduras insaturadas (ex: abacate, azeite de oliva, nozes)."
+    } else if(IMC >= 30 && IMC <= 34.9){
+      resultado3 = "Dieta rigorosamente hipocalórica com acompanhamento profissional.  Aumento do consumo de proteínas para preservar a massa muscular durante o emagrecimento (ex: frango, ovos, peixe). Evitar alimentos altamente processados e ricos em açúcares e gorduras trans. Refeições pequenas e frequentes, controlando porções."
+    }
+       document.getElementById("resultado2").innerHTML = `De acordo com seu IMC: ${ IMC } (${ resultado3 })`;
+    }
+  
